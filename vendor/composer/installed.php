a81477c2bd0a2c53d99d2a83eb94542acb218283
ViniciusOLucio <vinicius.olucio@gmail.com>
2024-11-18 08:03:00 -0300

    Primeiro commit do tema WordPress

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6fc072088bd8f0d19ce9c79f28d3e112d1690f40',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6fc072088bd8f0d19ce9c79f28d3e112d1690f40',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.7.4',
+            'version' => '6.7.4.0',
+            'reference' => 'd4adef47ca21c90e6483d59dcb9e5b1023696937',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
